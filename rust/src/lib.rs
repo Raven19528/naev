07d6433e0a2e7a56f83bf6efdccdd5c7164daa9c
Edgar Simo-Serra <bobbens@gmail.com>
2024-10-04 17:37:36 +0900

    Use colour rust crate for naev rust as a test.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -10,7 +10,10 @@ fn ticks() -> i32 {
     return unsafe{SDL_GetTicks()};
 }
 
+extern crate colour;
+use colour::*;
+
 #[no_mangle]
 extern "C" fn greet() {
-    println!("Hello from Rust code! {}", ticks());
+    green_ln!("Hello from Rust code! {}", ticks());
 }
